3508f9f8a30069deb852bdab7452ed2bacbb12b7
Sergey Polischuk <spolischook@gmail.com>
2013-11-21 12:12:35 +0200

    Generated DreamBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
+            new GeekHub\DreamBundle\DreamBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/GeekHub/DreamBundle/DreamBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GeekHub\DreamBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DreamBundle extends Bundle
+{
+}
