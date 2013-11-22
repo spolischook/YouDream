6f81a3aa32028079dcc95bd22bb40845d5c47c90
Sergey Polischuk <spolischook@gmail.com>
2013-11-22 12:52:58 +0200

    Added HautelookAliceBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,9 +18,11 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
-            new GeekHub\DreamBundle\DreamBundle(),
-            new GeekHub\UserBundle\GeekHubUserBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Hautelook\AliceBundle\HautelookAliceBundle(),
+            new GeekHub\DreamBundle\DreamBundle(),
+            new GeekHub\UserBundle\GeekHubUserBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
