4809a86922d883d9cab296192b18415dd97c5e26
Sergey Polischuk <spolischook@gmail.com>
2013-11-27 16:07:20 +0200

    Add simple reload

---
+++ bin/reload.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__.'/base_script.php';
+
+build_bootstrap();
+
+show_run("database:drop", "app/console doctrine:mongodb:schema:drop");
+show_run("schema:create", "app/console doctrine:mongodb:schema:create");
+
+show_run("Destroying cache dir manually", "rm -rf app/cache/*");
+
+show_run("Creating directories for app kernel", "mkdir -p app/cache/dev app/cache/test app/logs web/uploads");
+
+show_run("Warming up dev cache", "php app/console --env=dev cache:clear --no-warmup");
+//show_run("Warming up test cache", "php app/console --env=test cache:clear");
+
+show_run("Changing permissions", "chmod -R 777 app/cache app/logs web/uploads");
+show_run("fixtures:load", "app/console doctrine:mongodb:fixtures:load --no-interaction --fixtures=/var/www/chedream3/app/Resources/DataFixtures");
+
+show_run("assets:install", "app/console assets:install --symlink");
+show_run("assets:dump", "app/console assetic:dump");
+//fix
+show_run("Warming up dev cache", "php app/console --env=dev cache:clear --no-warmup");
+show_run("Changing permissions", "chmod -R 777 app/cache app/logs web/uploads");
+
+exit(0);
