82b33d3e77f4d8f637fc797793353d137a3a24e6
Sergey Polischuk <spolischook@gmail.com>
2013-12-01 02:23:50 +0200

    Updated Fixtures

--- app/Resources/DataFixtures/MongoDB/DataFixtureLoader.php
+++ app/Resources/DataFixtures/MongoDB/DataFixtureLoader.php
@@ -1,11 +1,23 @@
 <?php
 
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Hautelook\AliceBundle\Alice\DataFixtureLoader as BaseLoader;
 use Nelmio\Alice\Fixtures;
-use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use GeekHub\MediaBundle\Document\EmbeddedImage;
 
-class DataFixtureLoader extends BaseLoader
+class DataFixtureLoader extends BaseLoader implements ContainerAwareInterface
 {
+    /** @var ContainerInterface */
+    protected $container;
+
+    /** {@inheritDoc} */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     protected function getFixtures()
     {
         return  array(
@@ -17,5 +29,33 @@ class DataFixtureLoader extends BaseLoader
     public function getImagesDir()
     {
         return __DIR__ . '/data/images/';
+    }
+
+    public function setImage($originalName)
+    {
+        $path = __DIR__ . '/data/images/';
+        $targetFile = $this->getUploadPath() . $originalName;
+        $this->container->get('filesystem')->copy($path.$originalName, $targetFile);
+
+
+        $img = new EmbeddedImage();
+        $img->setPath($this->getRootUploadPath() . $originalName);
+
+        return $img;
+    }
+
+    public function getUploadPath()
+    {
+        return __DIR__ . '/../../../../web' . $this->getRootUploadPath();
+    }
+
+    public function getRootUploadPath()
+    {
+        return '/uploads/';
+    }
+
+    public function setRandomDreamImage()
+    {
+        return $this->setImage(rand(1,10).'.jpg');
     }
 }
